eb84e01d87d956155dbe5c35f1d6eb52811457f3
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-06-14 17:41:29 +0200

    strict equal added

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -4,7 +4,7 @@ const BlogDetails = () => {
     const { id } = useParams();// nutzt die variable aus dem Pfad /item/:id bzw item{props.id} -- 
     const location = useLocation();// nimmt den state, der auf dem Link (Pfad) übergeben wird
     const data = location.state;
-    const dataFilter = data.filter((ele) => ele.id == id)[0];// ele steht bei filter() für jedes Objekt im Array -- filter() gibt immer ein Array zurück! -- mit [0] wird in diesem Fall das einzige Objekt wiedergeben 
+    const dataFilter = data.filter((ele) => ele.id === id)[0];// ele steht bei filter() für jedes Objekt im Array -- filter() gibt immer ein Array zurück! -- mit [0] wird in diesem Fall das einzige Objekt wiedergeben 
 
     console.log(dataFilter);
     return (
